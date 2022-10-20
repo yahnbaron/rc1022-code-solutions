0d3778948485110d4b4a901699ee2404021cd29e
Baron Yahn <yahnbaron@me.com>
2022-10-20 02:31:24 +0000

    Completed javascript-loops exercise.

---
+++ javascript-loops/main.js
@@ -0,0 +1,70 @@
+/* exported getNumbersToTen,
+            getEvenNumbersToTwenty,
+            repeatWord,
+            logEachCharacter,
+            doubleAll,
+            getKeys,
+            getValues
+ */
+function getNumbersToTen() {
+  var numbers = [];
+  var currentNumber = 1;
+  while (currentNumber < 11) {
+    numbers.push(currentNumber);
+    currentNumber++;
+  }
+  return numbers;
+}
+
+function getEvenNumbersToTwenty() {
+  var evenNumbers = [];
+  var currentNumber = 2;
+  while (currentNumber < 21) {
+    evenNumbers.push(currentNumber);
+    currentNumber += 2;
+  }
+  return evenNumbers;
+}
+
+function repeatWord(word, times) {
+  var count = 1;
+  var repeated = '';
+  while (count < (times + 1)) {
+    repeated += word;
+    count++;
+  }
+  return repeated;
+}
+
+function logEachCharacter(string) {
+  for (var i = 0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+}
+
+function doubleAll(numbers) {
+  var doubled = [];
+  for (var i = 0; i < numbers.length; i++) {
+    doubled.push(numbers[i] * 2);
+  }
+  return doubled;
+}
+
+function getKeys(object) {
+  var keys = [];
+  for (var x in object) {
+    // console.log(x);
+    // keys.push(object[x]);
+    keys.push(x);
+  }
+  // console.log(x);
+  return keys;
+}
+
+function getValues(object) {
+  var values = [];
+  for (var x in object) {
+    values.push(object[x]);
+  }
+  return values;
+}
