4bdbdb3f57878071600ca4b41761e9d4e2d3e13a
Baron Yahn <yahnbaron@me.com>
2022-10-24 19:17:18 +0000

    Removed redundant code in compact.js.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,17 +1,10 @@
 /* exported compact */
 function compact(array) {
   var newArray = [];
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] !== false && array[i] !== null && array[i] !== 0 &&
-      array[i] !== '-0' && array[i] !== undefined && array[i] !== '') {
-      newArray.push(array[i]);
+  for (var x = 0; x < array.length; x++) {
+    if (array[x]) {
+      newArray.push(array[x]);
     }
   }
-  var newerArray = [];
-  for (var x = 0; x < newArray.length; x++) {
-    if (newArray[x]) {
-      newerArray.push(newArray[x]);
-    }
-  }
-  return newerArray;
+  return newArray;
 }
