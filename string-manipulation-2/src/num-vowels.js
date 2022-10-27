9c6f667eae084be078c93cb53af8a9f220197c86
Baron Yahn <yahnbaron@me.com>
2022-10-27 19:09:04 +0000

    wrote numVowels function.

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,11 @@
 /* exported numVowels */
+function numVowels(string) {
+  var vowCount = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] === 'a' || string[i] === 'e' || string[i] === 'i' || string[i] === 'o' || string[i] === 'u' ||
+      string[i] === 'A' || string[i] === 'E' || string[i] === 'I' || string[i] === 'O' || string[i] === 'U') {
+      vowCount++;
+    }
+  }
+  return vowCount;
+}
