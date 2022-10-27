9b3cc7f4f190cfef496bcaef7fdf51540b0ef2b5
Baron Yahn <yahnbaron@me.com>
2022-10-27 05:32:30 +0000

    saving progress

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,13 @@
 /* exported capitalizeWord */
+function capitalizeWord(word) {
+  if (word.toLowerCase() === 'javascript') {
+    return 'JavaScript';
+  } else {
+    var newWord = '';
+    newWord += word[0].toUpperCase();
+    for (var i = 1; i < word.length; i++) {
+      newWord += word[i].toLowerCase();
+    }
+    return newWord;
+  }
+}
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,13 @@
 /* exported ransomCase */
+function ransomCase(string) {
+  var lowString = string.toLowerCase();
+  var returnString = lowString[0];
+  for (var i = 1; i < string.length; i++) {
+    if (i % 2 === 0) {
+      returnString += lowString[i];
+    } else {
+      returnString += lowString[i].toUpperCase();
+    }
+  }
+  return returnString;
+}
