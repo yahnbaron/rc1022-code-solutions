b2cf9ad59f70dfe95a4a3347ed938f05acfdac21
Baron Yahn <yahnbaron@me.com>
2022-10-27 19:16:04 +0000

    wrote firstChars function.

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,14 @@
 /* exported firstChars */
+function firstChars(length, string) {
+  var retString = '';
+  var smartLength = 0;
+  if (string.length < length) {
+    smartLength = string.length;
+  } else {
+    smartLength = length;
+  }
+  for (var i = 0; i < smartLength; i++) {
+    retString += string[i];
+  }
+  return retString;
+}
