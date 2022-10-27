6c9b0ced686bfc57e79965b16948ac51d7bca92e
Baron Yahn <yahnbaron@me.com>
2022-10-27 21:04:16 +0000

    saving progress before moving to view-swap assignment.

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,16 @@
 /* exported lastChars */
+function lastChars(length, string) {
+  var retString = '';
+  var smartLength = 0;
+  if (string.length < length) {
+    smartLength = string.length;
+  } else {
+    smartLength = length;
+  }
+
+  var remainder = (string.length - smartLength);
+  for (var i = remainder; i < smartLength; i++) {
+    retString += string[i];
+  }
+  return retString;
+}
