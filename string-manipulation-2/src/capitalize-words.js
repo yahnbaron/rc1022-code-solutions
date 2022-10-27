e663ef9a03a617a2c84f5bc64fe5d855416c7cc2
Baron Yahn <yahnbaron@me.com>
2022-10-27 18:58:29 +0000

    wrote capitalizeWords function.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,12 @@
 /* exported capitalizeWords */
+function capitalizeWords(string) {
+  var returnString = string[0].toUpperCase();
+  for (var i = 1; i < string.length; i++) {
+    if (string[i - 1] === ' ') {
+      returnString += string[i].toUpperCase();
+    } else {
+      returnString += string[i].toLowerCase();
+    }
+  }
+  return returnString;
+}
