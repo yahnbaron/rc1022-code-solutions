f5656e7b9fc4624fa6f6cd1cfd88b2d8e86d68e6
Baron Yahn <yahnbaron@me.com>
2022-10-28 19:17:43 +0000

    Completed javascript-local-storage exercise.

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,13 @@
+/* exported todos */
+
+var todos = [];
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
+window.addEventListener('beforeunload', storeEm);
+
+function storeEm(event) {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+}
