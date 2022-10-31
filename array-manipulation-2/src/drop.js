fac3d5d2c5b79d4f733e90d20818bb33c87e8fcc
Baron Yahn <yahnbaron@me.com>
2022-10-31 01:31:57 +0000

    finished the drop function.

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,1 +1,12 @@
 /* exported drop */
+
+function drop(array, count) {
+  var newArray = [];
+  if (array.length === 0) {
+    return newArray;
+  }
+  for (var startPoint = count; startPoint < array.length; startPoint++) {
+    newArray.push(array[startPoint]);
+  }
+  return newArray;
+}
