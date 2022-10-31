6ec02b5f309ef34459dae6106580d13d2b79c3af
Baron Yahn <yahnbaron@me.com>
2022-10-31 02:11:12 +0000

    finished dropRight function.

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,11 @@
 /* exported dropRight */
+function dropRight(array, count) {
+  var newArray = [];
+  if (array.length < count) {
+    return array;
+  }
+  for (var i = 0; i < array.length - count; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
