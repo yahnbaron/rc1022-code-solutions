b2bd23be5099d6564e66dbfccf89d39268ce02f5
Baron Yahn <yahnbaron@me.com>
2022-11-07 21:57:10 +0000

    saving progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,12 @@
 /* exported omit */
+function omit(source, keys) {
+  var newObj = {};
+  for (var i = 0; i < keys.length; i++) {
+    for (var keez in source) {
+      if (keys[i] !== keez && source[keez] !== undefined) {
+        newObj[keez] = source[keez];
+      }
+    }
+  }
+  return newObj;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,4 +1,8 @@
-/* exported pick */
+/* exported pick
+  create new empty object
+  for every index in keys, check if its one of the properties of source
+  if it is, put that key and its value in the new object and return it
+*/
 function pick(source, keys) {
   var newObj = {};
   for (var i = 0; i < keys.length; i++) {
@@ -10,16 +14,3 @@ function pick(source, keys) {
   }
   return newObj;
 }
-
-/* function pick(source, keys) {
-  var newObj = {};
-  for (var keez in source) {
-    for (var i = 0; i < keys.length; i++) {
-      if (keys[i] === keez) {
-        newObj[keez] = source[keez];
-      }
-    }
-  }
-  return newObj;
-}
-*/
