020ca687ca4e149e9c53e874be1a92b378251921
Baron Yahn <yahnbaron@me.com>
2022-11-07 19:56:45 +0000

    saving progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -2,10 +2,13 @@
 function pick(source, keys) {
   var newObj = {};
   for (var i = 0; i < keys.length; i++) {
+    // console.log(i, keys[i]);
     for (var keez in source) {
       if (keys[i] === keez) {
-        newObj[keys] = source[keez];
+        newObj[keez] = source[keez];
+        // console.log('newObj', newObj);
       }
     }
   }
+  return newObj;
 }
