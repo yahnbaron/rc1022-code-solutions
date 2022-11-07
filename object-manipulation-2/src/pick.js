05eadf2a7d623208e57aff4f30ed5b0924e3eaf2
Baron Yahn <yahnbaron@me.com>
2022-11-07 21:55:14 +0000

    finished the pick function 1/4

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -2,13 +2,24 @@
 function pick(source, keys) {
   var newObj = {};
   for (var i = 0; i < keys.length; i++) {
-    // console.log(i, keys[i]);
     for (var keez in source) {
-      if (keys[i] === keez) {
+      if (keys[i] === keez && source[keez] !== undefined) {
         newObj[keez] = source[keez];
-        // console.log('newObj', newObj);
       }
     }
   }
   return newObj;
 }
+
+/* function pick(source, keys) {
+  var newObj = {};
+  for (var keez in source) {
+    for (var i = 0; i < keys.length; i++) {
+      if (keys[i] === keez) {
+        newObj[keez] = source[keez];
+      }
+    }
+  }
+  return newObj;
+}
+*/
