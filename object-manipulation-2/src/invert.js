4d0c2462363d95b62f09e7dd64cfbffdaff3ba6e
Baron Yahn <yahnbaron@me.com>
2022-11-08 00:54:26 +0000

    saving progress on invert

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,11 @@
-/* exported invert */
+/* exported invert
+  loop over each key in source and make newObj[key] equal to key, then return that object
+*/
+function invert(source) {
+  var newObj = {};
+  for (var key in source) {
+    newObj[key] = source[key];
+  }
+  // console.log(newObj);
+  return newObj;
+}
