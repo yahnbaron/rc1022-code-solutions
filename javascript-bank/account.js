d0fc51133e44fd170465d6fda87bfb8948f081d2
Baron Yahn <yahnbaron@me.com>
2022-11-08 20:15:47 +0000

    Finished the first two constructors.

---
+++ javascript-bank/account.js
@@ -0,0 +1,43 @@
+/* exported Account */
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+Account.prototype.deposit = function (amount) {
+  if (amount > 0 && Number.isInteger(amount)) {
+    this.transactions.push(new Transaction('deposit', amount));
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (amount > 0 && Number.isInteger(amount)) {
+    this.transactions.push(new Transaction('withdrawal', amount));
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.getBalance = function () {
+  var tally = 0;
+  if (this.transactions.length === 0) {
+    return tally;
+  } else {
+    var totalDeposits = 0;
+    var totalWithdrawals = 0;
+    for (var i = 0; i < this.transactions.length; i++) {
+      if (this.transactions[i].type === 'deposit') {
+        totalDeposits += this.transactions[i].amount;
+      } else {
+        totalWithdrawals += this.transactions[i].amount;
+      }
+    }
+    tally = totalDeposits - totalWithdrawals;
+    return tally;
+  }
+};
