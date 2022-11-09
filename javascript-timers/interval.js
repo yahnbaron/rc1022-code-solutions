f130e047403f74ca3580aad58225e5f2f890888d
Baron Yahn <yahnbaron@me.com>
2022-11-09 18:21:38 +0000

    Updated code to be more logical and less verbose.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,15 +1,13 @@
 var $countdown = document.querySelector('.countdown-display');
+var countdown = parseInt($countdown.textContent);
 
 var intervalId = setInterval(time, 1000);
 
 function time() {
-  if ($countdown.textContent === '4') {
-    $countdown.textContent = '3';
-  } else if ($countdown.textContent === '3') {
-    $countdown.textContent = '2';
-  } else if ($countdown.textContent === '2') {
-    $countdown.textContent = '1';
-  } else if ($countdown.textContent === '1') {
+  if (countdown > 0) {
+    $countdown.textContent = countdown;
+    countdown--;
+  } else {
     $countdown.textContent = '~Earth Beeeelooowww Us~';
     clearInterval(intervalId);
   }
