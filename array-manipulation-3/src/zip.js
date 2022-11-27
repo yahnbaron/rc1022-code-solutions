a2a7be08cbcdc812c33897632a4f6fc12ab2b72f
Baron Yahn <yahnbaron@me.com>
2022-11-27 21:25:20 +0000

    created zip function

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,20 @@
-/* exported zip */
+/* exported zip
+  create a new return array, then establish length to loop
+  loop through length and make a new array for a pair to be pushed into
+  finally push the pair array into the return array */
+function zip(first, second) {
+  var pairedUp = [];
+  var howMany = 0;
+  if (first.length < second.length) {
+    howMany = first.length;
+  } else {
+    howMany = second.length;
+  }
+  for (var i = 0; i < howMany; i++) {
+    var pair = [];
+    pair.push(first[i]);
+    pair.push(second[i]);
+    pairedUp.push(pair);
+  }
+  return pairedUp;
+}
