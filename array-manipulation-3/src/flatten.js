0c963c728ffc0300ddc7a9d4c53a5e03378c69da
Baron Yahn <yahnbaron@me.com>
2022-11-27 20:58:32 +0000

    completed flatten function

--- array-manipulation-3/src/flatten.js
+++ array-manipulation-3/src/flatten.js
@@ -1,1 +1,6 @@
-/* exported flatten */
+/* exported flatten
+  return a new array being concatenated with the expanded array via spread syntax
+  since spread syntax only goes one level deep */
+function flatten(array) {
+  return [].concat(...array);
+}
