4bee5eb097c6b502b226e69d364d27eba68809c3
Baron Yahn <yahnbaron@me.com>
2022-11-27 21:38:58 +0000

    completed unique function

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,12 @@
-/* exported unique */
+/* exported unique
+  create a new array to return, then at each index of the array check
+  if the word already exists in the new array, pushing value at index to new array if it doesnt */
+function unique(array) {
+  var returnArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (returnArray.indexOf(array[i]) === -1) {
+      returnArray.push(array[i]);
+    }
+  }
+  return returnArray;
+}
