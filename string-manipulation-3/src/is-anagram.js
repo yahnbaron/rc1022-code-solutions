fa118fd4a1a6a48ea69ba8108bf44fbe64e605c6
Baron Yahn <yahnbaron@me.com>
2022-11-27 00:19:10 +0000

    saving progress after finishing isAnagram function (minus one check)

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,3 +1,27 @@
 /* exported isAnagram
-  Create new
+  Create new strings without spaces, then compare the length comparing false if different
+  for every digit in first spaceless string, check if second includes it
 */
+function isAnagram(firstString, secondString) {
+  var noSpaceOne = '';
+  var noSpaceTwo = '';
+  for (var i = 0; i < firstString.length; i++) {
+    if (firstString[i] !== ' ') {
+      noSpaceOne += firstString[i];
+    }
+  }
+  for (i = 0; i < secondString.length; i++) {
+    if (secondString[i] !== ' ') {
+      noSpaceTwo += secondString[i];
+    }
+  }
+  if (noSpaceOne.length !== noSpaceTwo.length) {
+    return false;
+  }
+  for (i = 0; i < noSpaceOne.length; i++) {
+    if (!(noSpaceOne.includes(noSpaceTwo[i]))) {
+      return false;
+    }
+  }
+  return true;
+}
