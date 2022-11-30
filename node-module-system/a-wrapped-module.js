f849bf154d84175d44ee191075cfecf4814d3378
Baron Yahn <yahnbaron@me.com>
2022-11-30 00:30:45 +0000

    added missing require typeof and value.

--- node-module-system/a-wrapped-module.js
+++ node-module-system/a-wrapped-module.js
@@ -1,5 +1,7 @@
 console.log('typeof exports: ', typeof exports);
 console.log('value of exports: ', exports);
+console.log('typeof require: ', typeof require);
+console.log('value of require: ', require);
 console.log('typeof module: ', typeof module);
 console.log('value of module: ', module);
 console.log('typeof __filename: ', typeof __filename);
