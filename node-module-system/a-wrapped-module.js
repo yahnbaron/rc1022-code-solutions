d29e4f317ac323d67473d1b31b5763cd609ea42d
Baron Yahn <yahnbaron@me.com>
2022-11-30 00:24:01 +0000

    completed node-module-system exercise

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,8 @@
+console.log('typeof exports: ', typeof exports);
+console.log('value of exports: ', exports);
+console.log('typeof module: ', typeof module);
+console.log('value of module: ', module);
+console.log('typeof __filename: ', typeof __filename);
+console.log('value of __filename: ', __filename);
+console.log('typeof __dirname: ', typeof __dirname);
+console.log('value of __dirname: ', __dirname);
