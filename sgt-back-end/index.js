86e39085dd19bcb63d5e0128f2248584198475c4
Baron Yahn <yahnbaron@me.com>
2022-12-08 21:59:13 +0000

    completed get route

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,12 +1,35 @@
-// const pg = require('pg');
+const pg = require('pg');
 const express = require('express');
 const app = express();
 const json = express.json();
 app.use(json);
 
-// const db = new pg.Pool({
-//  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
-//  ssl: {
-//    rejectUnauthorized: false
-//  }
-// });
+app.listen(3000, () => process.stdout.write('Listening on port 3000!'));
+
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.get('/api/grades', (req, res, next) => {
+  const sql = `
+    select "gradeId",
+           "name",
+           "course",
+           "score",
+           "createdAt"
+      from "grades"
+  `;
+  db.query(sql)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
