d7885aa8f666311d6ae70d54500483fc1da72dea
Baron Yahn <yahnbaron@me.com>
2022-12-09 18:45:34 +0000

    completed evenNumbers

---
+++ array-filter/main.js
@@ -0,0 +1,15 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna'
+];
+
+const evenNumbers = numbers.filter(numbers => numbers % 2 === 0);
