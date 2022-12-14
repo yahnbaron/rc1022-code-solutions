b56d81be4a4fcd51a9112dde4ceb8bd711304dd3
Baron Yahn <yahnbaron@me.com>
2022-12-14 23:06:02 +0000

    completed react-events-and-state exercise

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { isClicked: false };
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    if (this.state.isClicked) {
+      return <button onClick={this.handleClick}>Thanks!</button>;
+    } else {
+      return <button onClick={this.handleClick}>{this.props.text}</button>;
+    }
+  }
+}
+
+const element = (
+  <div>
+    <CustomButton text="Click Me!" />
+  </div>
+);
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(element);
