59339ef03a78321956f0efec6730a90018abb193
Baron Yahn <yahnbaron@me.com>
2022-12-19 20:27:07 +0000

    fixed props passing

--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -4,5 +4,10 @@ import Accordion from './accordion';
 
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
+const topics = [
+  { topic: 'Hypertext Markup Language', details: 'The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.' },
+  { topic: 'Cascading Style Sheets', details: 'Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript. CSS is designed to enable the separation of content and presentation, including layout, colors, and fonts.' },
+  { topic: 'JavaScript', details: 'JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries.' }
+];
 
-root.render(<Accordion />);
+root.render(<Accordion topics={topics} />);
