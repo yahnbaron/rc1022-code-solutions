fdad1b304962e262cafcc0154e8c3a201b14be4f
Baron Yahn <yahnbaron@me.com>
2022-12-20 05:48:16 +0000

    completed react-carousel-component exercise

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,14 +3,70 @@ import React from 'react';
 export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentImage: 1 };
+
+    this.state = {
+      currentIndex: 0
+    };
+
+    this.handlePrevClick = this.handlePrevClick.bind(this);
+    this.handleNextClick = this.handleNextClick.bind(this);
+    this.handleDotClick = this.handleDotClick.bind(this);
+    this.startTimer();
   }
 
-  handleClick() {
-    this.setState({ currentImage: this.state.currentImage + 1 });
+  startTimer() {
+    this.timerId = setInterval(() => {
+      this.handleNextClick();
+    }, 3000);
+  }
+
+  handlePrevClick() {
+    this.setState(prevState => ({
+      currentIndex: (prevState.currentIndex + this.props.images.length - 1) % this.props.images.length
+    }));
+    clearInterval(this.timerId);
+    this.startTimer();
+  }
+
+  handleNextClick() {
+    this.setState(prevState => ({
+      currentIndex: (prevState.currentIndex + 1) % this.props.images.length
+    }));
+    clearInterval(this.timerId);
+    this.startTimer();
+  }
+
+  handleDotClick(index) {
+    this.setState({ currentIndex: index });
+    clearInterval(this.timerId);
+    this.startTimer();
   }
 
   render() {
-    return <div></div>;
+    const currentImage = this.props.images[this.state.currentIndex];
+    return (
+      <div className="container">
+        <div className="row gallery align-center space-between">
+          <div className="col-auto">
+            <i className="fa-solid fa-chevron-left left-button" onClick={this.handlePrevClick}></i>
+          </div>
+          <div className="col-auto">
+            <img src={currentImage}></img>
+          </div>
+          <div className="col-auto">
+            <i className="fa-solid fa-chevron-right right-button" onClick={this.handleNextClick}></i>
+          </div>
+        </div>
+        <div className="row dots">
+          {this.props.images.map((image, index) => (
+            <div key={index} className="col-auto">
+              <span className={`fa-solid circle${index} fa-${index === this.state.currentIndex ? 'circle' : 'o'}`}
+                onClick={() => this.handleDotClick(index)}>
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
