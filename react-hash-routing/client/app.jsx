527a0fc2f874e96d01daac690386845829b57469
Baron Yahn <yahnbaron@me.com>
2022-12-22 03:38:32 +0000

    completed react-hash-routing exercise

---
+++ react-hash-routing/client/app.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Header from './components/header';
+import Catalog from './pages/catalog';
+import NotFound from './pages/not-found';
+import ProductDetails from './pages/product-details';
+import { parseRoute } from './lib';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', event => {
+      const route = parseRoute(window.location.hash);
+      this.setState({ route });
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    if (route.path === '') {
+      return <Catalog />;
+    }
+    if (route.path === 'products') {
+      const productId = route.params.get('productId');
+      return <ProductDetails productId={productId} />;
+    }
+    return <NotFound />;
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        { this.renderPage() }
+      </>
+    );
+  }
+}
